9f1a123f751fd7af843d0e34e331d97eb5a18708
Mason Chansamone <mach3999@colorado.edu>
2026-07-28 22:01:19 +0000

    Created db connection

--- app.py
+++ app.py
@@ -1,5 +1,31 @@
 from flask import Flask
+import psycopg2
+import os
+
 app = Flask(__name__)
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+conn = None
+cur = None
+
+try:
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+
+    # SQL work goes here
+
+    conn.commit()
+    return "Success message here"
+except Exception as e:
+    if conn is not None:
+        conn.rollback()
+    return f"Database error: {e}"
+finally:
+    if cur is not None:
+        cur.close()
+    if conn is not None:
+        conn.close()
+
 
 @app.route("/")
 def index():
