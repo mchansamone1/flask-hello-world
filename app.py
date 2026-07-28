0fbf346dcdc915d2d3cd61b2a387166d902cbac0
Mason Chansamone <mach3999@colorado.edu>
2026-07-28 22:05:35 +0000

    /db_test route

--- app.py
+++ app.py
@@ -8,25 +8,37 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 conn = None
 cur = None
 
-try:
-    conn = psycopg2.connect(DATABASE_URL)
-    cur = conn.cursor()
+# try:
+#     conn = psycopg2.connect(DATABASE_URL)
+#     cur = conn.cursor()
 
-    # SQL work goes here
+#     # SQL work goes here
 
-    conn.commit()
-    return "Success message here"
-except Exception as e:
-    if conn is not None:
-        conn.rollback()
-    return f"Database error: {e}"
-finally:
-    if cur is not None:
-        cur.close()
-    if conn is not None:
-        conn.close()
+#     conn.commit()
+#     return "Success message here"
+# except Exception as e:
+#     if conn is not None:
+#         conn.rollback()
+#     return f"Database error: {e}"
+# finally:
+#     if cur is not None:
+#         cur.close()
+#     if conn is not None:
+#         conn.close()
 
 
 @app.route("/")
 def index():
     return "Hello World from Mason Chansamone in 3308"
+
+@app.route("/db_test")
+def db_test():
+    conn = None
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        return "Database connection successful"
+    except Exception as e:
+        return f"Database connection failed: {e}"
+    finally:
+        if conn is not None:
+            conn.close()
